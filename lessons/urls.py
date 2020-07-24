d90ca17461ee51d42a48d53e29bd19bf100fae06
Maksim <maxim.park1235@gmail.com>
2020-07-24 22:48:16 +0500

    details/result views added. comment posting not working

--- lessons/urls.py
+++ lessons/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'lessons'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>', views.DetailView.as_view(), name='details')
+    path('<int:pk>', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/result', views.ResultView.as_view(), name='result'),
+    path('<int:lesson_id>/comment/', views.comment, name='comment'),
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from .models import Lesson, Comment
 
@@ -22,8 +23,25 @@ class DetailView(generic.DetailView):
         return Lesson.objects.all()
 
 
+class ResultView(generic.DetailView):
+    model = Lesson
+    template_name = 'lessons/result.html'
+
+
 def comment(request, lesson_id):
-    lesson = get_object_or_404(Lesson, lesson_id)
+    lesson = get_object_or_404(Lesson, pk=lesson_id)
 
     # Need to obtain comment_text from form and then assign it to lesson variable
     # declared above. Once it is done, save it with lesson.save()
+    try:
+        """ selected_lesson = request.POST['lesson_id'] """
+    except (KeyError, Comment.DoesNotExist):
+        return render(request, 'lessons:detail.html', {
+            'lesson': lesson,
+            'error_message': "You didn't fill comment section",
+        })
+    else:
+        lesson.comment_text = request.POST['comment_text']  # Gives no value
+        lesson.save()
+
+        return HttpResponseRedirect(reverse('lessons:result', args=(lesson_id,)))
