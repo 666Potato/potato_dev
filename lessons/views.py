14bbc3129ead3d645c0302de3d14499642cf7fa5
Maksim <maxim.park1235@gmail.com>
2020-08-01 01:34:00 +0500

    ajax posting

--- lessons/views.py
+++ lessons/views.py
@@ -3,8 +3,10 @@ from django.views import generic
 from django.http import HttpResponse, JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core import serializers
+from django.utils.decorators import method_decorator
 
 from .models import Lesson, Comment
 
@@ -18,6 +20,7 @@ class IndexView(LoginRequiredMixin, generic.ListView):
         return Lesson.objects.all()
 
 
+# @method_decorator(csrf_exempt, name='dispatch')
 class DetailView(LoginRequiredMixin, generic.DetailView):
     model = Lesson
     template_name = 'lessons/left-sidebar.html'
@@ -32,14 +35,14 @@ class ResultView(LoginRequiredMixin, generic.DetailView):
 
 
 @login_required
+@csrf_exempt
 def comment(request, lesson_id):
     lesson = get_object_or_404(Lesson, pk=lesson_id)
 
     # Object created
-    new_comment = Comment.objects.create(topic=lesson, comment_text=request.POST['comment_text'], posted_by=request.user)
+    new_comment = Comment.objects.create(topic=lesson, comment_text=request.POST['comment_text'],
+                                         posted_by=request.user)
 
     # Create queryset and transform into json
-
-    qs = lesson.comment_set.filter(pk=new_comment.pk)
-    qs_json = serializers.serialize('json', qs)
-    return HttpResponse(qs_json, content_type='application/json')
+    user = request.user.get_username()
+    return JsonResponse({"comment_text": new_comment.comment_text, "posted_by": user})
