42dc2627d44d4977f12e03b2a1cd08fa463a33e7
Maksim <maxim.park1235@gmail.com>
2020-08-07 04:41:21 +0500

    udalil chast' ribi / redirect

--- potato_dev/urls.py
+++ potato_dev/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
+    path('', views.index, name='redirect'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('lessons/', include('lessons.urls')),
     path('admin/', admin.site.urls),
